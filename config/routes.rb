eacd0261e863a4657fc137cb39b8790f1959f99d
charleysan <hambley1997@gmail.com>
2025-03-19 19:55:58 +1000

    added sessions and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-   resources :posts
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  resources :users
+  resources :posts
   resources :copics
 end
